4cbe3f843b0a89545e768d019b9a7065686ddc9a
Vladislav Bezborodyi <paster.lak@gmail.com>
2025-11-05 22:33:55 +0100

    Add - profiler. vscode tasks

---
+++ PineCone-Management-Dashboard/components/profiler.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+extern "C" {
+    #include <stdint.h>
+}
+
+
+class Profiler {
+public:
+    Profiler();
+    void start();
+    void end();
+    uint32_t getUs() const;
+    float getMs() const;
+    float getSec() const;
+    void printTime() const;
+
+private:
+    uint64_t startTime_us;
+    uint64_t endTime_us;
+    uint32_t elapsed_us;
+};
