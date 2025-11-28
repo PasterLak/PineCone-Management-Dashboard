0ceb08aafda067d23456cf4f17d300355f312991
Timkroe21 <tim.kroeckel@gmail.com>
2025-11-28 21:23:24 +0100

    WLAN_Demo

---
+++ PineCone-Management-Dashboard/components/WLANHandler.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+
+extern "C" {
+    #include <FreeRTOS.h>
+    #include <task.h>
+    #include <wifi_mgmr_ext.h>
+    #include <lwip/sockets.h>
+    #include <lwip/inet.h>
+    #include <lwip/netdb.h>
+}
+
+class WLANHandler {
+private:
+    const char* ssid;
+    const char* password;
+    bool wifi_initialized = false;
+public:
+    WLANHandler(const char* mySsid, const char* myPassword);
+
+    void start();
+
+    // KORRIGIERT: Nur ein "bool"
+    bool isConnected();
+
+    void sendData(const char* ip_address);
+
+    char* get_password();
+
+    char* get_ip_address();
+};
