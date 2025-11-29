72eca41c0b9e088054d766f22d3f63f7f6144b66
WenzelburgerMarc <54157548+WenzelburgerMarc@users.noreply.github.com>
2025-11-29 12:40:58 +0100

    comments

--- PineCone-Management-Dashboard/extentions/Print.hpp
+++ PineCone-Management-Dashboard/extentions/Print.hpp
@@ -1,78 +1,81 @@
 #pragma once
 
 extern "C" {
-    #include <stdio.h>
-    #include <stdint.h>
-    #include <stdbool.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 }
 
 // With template it is not possible to use cpp
 // I don't know why
 
 class Printer {
-public:
-    Printer(const char* separator = " ", const char* end_of_line = "\r\n"){
-        this->sep = separator;
-        this->end_of_line = end_of_line;
-    };
+ public:
+  Printer(const char* separator = " ", const char* end_of_line = "\r\n") {
+    this->sep = separator;
+    this->end_of_line = end_of_line;
+  };
 
-    // Print only Variables out
-    template <typename T>
-    void print(T t) {
-        printOne(t);
-    }
+  // Print only Variables out
+  template <typename T>
+  void print(T t) {
+    printOne(t);
+  }
 
-    template<typename T, typename... Args>
-    void print(T t, Args... args) {
-        printOne(t);
-        printf("%s", sep);
-        print(args...);
-    }
+  template <typename T, typename... Args>
+  void print(T t, Args... args) {
+    printOne(t);
+    printf("%s", sep);
+    print(args...);
+  }
 
-    // print out line
-    template <typename T>
-    void printl(T t) {
-        printOne(t);
-        printf(this->end_of_line);
-    }
+  // print out line
+  template <typename T>
+  void printl(T t) {
+    printOne(t);
+    printf(this->end_of_line);
+  }
 
-    template<typename T, typename... Args>
-    void printl(T t, Args... args) {
-        printOne(t);
-        printf("%s", sep);
-        printl(args...);
-    }
+  template <typename T, typename... Args>
+  void printl(T t, Args... args) {
+    printOne(t);
+    printf("%s", sep);
+    printl(args...);
+  }
 
-private:
-    private:
-    const char* sep;
-    const char* end_of_line;
+ private:
+ private:
+  const char* sep;
+  const char* end_of_line;
 
-    // Overload the printOne function to support different types to print out
-    void printOne(const char* val) { printf("%s", val); }
-    void printOne(char* val)       { printf("%s", val); }
-    void printOne(double val)      { printf("%f", val); }
-    void printOne(float val)       { printf("%f", static_cast<double>(val)); }
-    void printOne(bool val)        { printf("%s", val ? "true" : "false"); }
+  // Overload the printOne function to support different types to print out
+  void printOne(const char* val) { printf("%s", val); }
+  void printOne(char* val) { printf("%s", val); }
+  void printOne(double val) { printf("%f", val); }
+  void printOne(float val) { printf("%f", static_cast<double>(val)); }
+  void printOne(bool val) { printf("%s", val ? "true" : "false"); }
 
-    // Integers
-    // Integers
-    void printOne(int8_t val)      { printf("%d", static_cast<int>(val)); }
-    void printOne(int16_t val)     { printf("%d", static_cast<int>(val)); }
-    void printOne(int32_t val)     { printf("%d", static_cast<int>(val)); }
-    void printOne(int64_t val)     { printf("%lld", static_cast<long long>(val)); }
+  // Integers
+  void printOne(int8_t val) { printf("%d", static_cast<int>(val)); }
+  void printOne(int16_t val) { printf("%d", static_cast<int>(val)); }
+  void printOne(int32_t val) { printf("%d", static_cast<int>(val)); }
+  void printOne(int64_t val) { printf("%lld", static_cast<long long>(val)); }
 
-    void printOne(int val)         { printf("%d", val); }
+  void printOne(int val) { printf("%d", val); }
 
-    // Unsigned integers
-    void printOne(uint8_t val)     { printf("%u", static_cast<unsigned int>(val)); }
-    void printOne(uint16_t val)    { printf("%u", static_cast<unsigned int>(val)); }
-    void printOne(uint32_t val)    { printf("%u", static_cast<unsigned int>(val)); }
-    void printOne(uint64_t val)    { printf("%llu", static_cast<unsigned long long>(val)); }
+  // Unsigned integers
+  void printOne(uint8_t val) { printf("%u", static_cast<unsigned int>(val)); }
+  void printOne(uint16_t val) { printf("%u", static_cast<unsigned int>(val)); }
+  void printOne(uint32_t val) { printf("%u", static_cast<unsigned int>(val)); }
+  void printOne(uint64_t val) {
+    printf("%llu", static_cast<unsigned long long>(val));
+  }
 
-    void printOne(unsigned int val){ printf("%u", val); }
+  void printOne(unsigned int val) { printf("%u", val); }
 
-    // if type is not listed
-    template <typename T>
-    void printOne(T t) { printf("[type not supported]"); }
+  // if type is not listed
+  template <typename T>
+  void printOne(T t) {
+    printf("[type not supported]");
+  }
 };
