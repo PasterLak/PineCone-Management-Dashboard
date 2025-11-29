39e8361edcf3c052b546606e28a54d014b7b7b25
WenzelburgerMarc <54157548+WenzelburgerMarc@users.noreply.github.com>
2025-11-29 11:33:06 +0100

    stack size

--- PineCone-Management-Dashboard/include/Config.hpp
+++ PineCone-Management-Dashboard/include/Config.hpp
@@ -15,9 +15,9 @@ constexpr float DASHBOARD_UPDATE_INTERVAL_SEC = 1.0f;
 constexpr float LED_BLINK_INTERVAL_SEC = 2.5f;
 
 // Network Configuration
-constexpr const char* WIFI_SSID = "SSID_Name";
-constexpr const char* WIFI_PASSWORD = "PASSWORD";
-constexpr const char* DASHBOARD_SERVER_IP = "IP_OF_PI";
+constexpr const char* WIFI_SSID = "SSID_PI_AP";
+constexpr const char* WIFI_PASSWORD = "PASSWORD_PI_AP";
+constexpr const char* DASHBOARD_SERVER_IP = "IP_PI";  // e.g. 192.168.178.75
 constexpr uint16_t DASHBOARD_SERVER_PORT = 5000;
 
 }  // namespace Config
