8eca4669ce8ae0b84082ad40b576662fd8b05091
Vladislav Bezborodyi <paster.lak@gmail.com>
2025-12-09 18:24:00 +0100

    Better DashboardManager

--- PineCone-Management-Dashboard/components/dashboard_manager.hpp
+++ PineCone-Management-Dashboard/components/dashboard_manager.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
 #include "wifi_handler.hpp"
-
+#include "extentions/log.hpp"
 // Manages dashboard communication and state
 class DashboardManager {
  public:
   DashboardManager(WIFIHandler& wlan_handler, const char* server_ip,
                    uint16_t server_port, float update_interval_sec);
+
+  void setDebugEnabled(bool enabled) {
+      debug_enabled = enabled;
+
+  }
 
   bool update(float delta_time_sec);
   bool isConnected() const { return connected; }
@@ -19,6 +24,14 @@ class DashboardManager {
   float update_interval_sec;
   float time_accumulator;
   bool connected;
+  bool debug_enabled = true;
+
+  template <typename... Args>
+  void log(const char* fmt, Args... args) const {
+      if (debug_enabled) {
+          Log::println(fmt, args...);
+      }
+  }
 
   void sendDataToDashboard();
   void logDashboardInfo();
